617c6a0c1f306ed80117aa08079fb85df97b15df
s6264 <jahelgithub64@gmail.com>
2026-05-13 11:16:53 +0200

    Make auth on the prokject

---
+++ src/Components/Navbar.php
@@ -0,0 +1,5 @@
+<nav>
+    <ul>
+        <li><a href="/">Page principal</a></li>
+    </ul>
+</nav>
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,16 +5,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 
-use App\Http\Controllers\BookController;
 use App\Http\Controllers\ProfileController;
 
-Route::get("/", [HomeController::class, 'index']);
-Route::get("/quizz", [QuizzController::class, 'index']);
-Route::get("/user", [UserController::class, 'index']);
+Route::get("/", [HomeController::class, 'index'])->middleware(['auth']);
+Route::get("/quizz", [QuizzController::class, 'index'])->middleware(['auth']);
+Route::get("/user", [UserController::class, 'index'])->middleware(["auth"]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/login', function() {
+    return view("login");
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
