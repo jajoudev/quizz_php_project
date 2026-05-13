9532d13e07ea634487583d738ebf4442ea83c01e
s6264 <jahelgithub64@gmail.com>
2026-05-13 10:29:07 +0200

    Install breeze and configure some setup

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,6 +5,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\ProfileController;
+
 Route::get("/", [HomeController::class, 'index']);
 Route::get("/quizz", [QuizzController::class, 'index']);
 Route::get("/user", [UserController::class, 'index']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
